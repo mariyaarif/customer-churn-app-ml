371e505bbd7dc64638fcb10cbcaa9d5f4102d79d
mariyaarif <121035298+mariyaarif@users.noreply.github.com>
2026-08-10 06:59:05 -0700

    app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ st.set_page_config(
 # Load Model and Encoders
 @st.cache_resource
 def load_artifacts():
-    with open(r'model\customer_churn.pkl', 'rb') as f:
+    with open('model\customer_churn.pkl', 'rb') as f:
         model_data = pickle.load(f)
-    with open(r'model\encoders.pkl', 'rb') as f:
+    with open('model\encoders.pkl', 'rb') as f:
         encoders = pickle.load(f)
     return model_data['model'], model_data['features_name'], encoders
 
